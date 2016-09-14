b6014a0caffc617baa517ef75246fca92ae9c115
Sudeep Reddy Gaddam <sudeepgaddam@ufl.edu>
2016-09-14 01:13:57 -0400

    Update power_of_three.cpp

--- leetcode/easy/power_of_three.cpp
+++ leetcode/easy/power_of_three.cpp
@@ -21,3 +21,14 @@ public:
         return (isPowerOfThree(n/3));
     }
 };
+
+/*
+72ms
+*/
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return fmod(log10(n)/log10(3), 1)==0;
+    }
+};
