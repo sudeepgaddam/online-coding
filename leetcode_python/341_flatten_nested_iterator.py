86b079bacc4c237932dd1ea66ea074aaea27d1fd
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-03-11 18:54:40 -0700

    Update 341_flatten_nested_iterator.py

--- leetcode_python/341_flatten_nested_iterator.py
+++ leetcode_python/341_flatten_nested_iterator.py
@@ -30,19 +30,48 @@ class NestedIterator(object):
         Initialize your data structure here.
         :type nestedList: List[NestedInteger]
         """
-        
+        self.nestedStack = [[nestedList,0]]
 
     def next(self):
         """
         :rtype: int
         """
-        
+        # print len(self.nestedStack[-1])
+        self.hasNext()
+        ll, i = self.nestedStack[-1]
+        num = ll[i].getInteger()
+        # print(num)
+        self.nestedStack[-1][1]+=1
+        return num
+        # if self.nestedStack[-1][1] < len(self.nestedStack[-1][0]):
+        #     if self.nestedStack[-1][0][self.currInd].isInteger():
+        #         num = self.nestedStack[-1][0][self.currInd].getInteger()
+        #         self.currInd += 1
+        #         return num   
+        #     else:
+        #         nestedPush(self.nestedStack[-1][0][self.currInd])
+
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        
+        # i = 0
+        while self.nestedStack:
+            # i+=1
+            ll, i = self.nestedStack[-1]
+            if i >= len(ll):
+                # print("Popping")
+                self.nestedStack.pop()
+            else:
+                if ll[i].isInteger():
+                    # print("Integer", ll[i].getInteger())
+                    return True
+                else:
+                    # print("Appending",ll[i].getList())
+                    self.nestedStack[-1][1] += 1
+                    self.nestedStack.append([ll[i].getList(),0])
+        return False
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
