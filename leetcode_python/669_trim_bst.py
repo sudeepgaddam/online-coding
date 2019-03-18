d2b109decadf74bcd3878c69503d1e6450c9f32f
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-03-18 07:42:50 -0700

    Create 669_trim_bst.py

---
+++ leetcode_python/669_trim_bst.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+        if root.val < L:
+            return self.trimBST(root.right,L,R)
+        if root.val > R:
+            return self.trimBST(root.left,L,R)
+        if root.right and root.right.val>R:
+            root.right = root.right.left
+        if root.left and root.left.val < L:
+            root.left = root.left.right
+        root.left = self.trimBST(root.left,L,R)
+        root.right  = self.trimBST(root.right,L,R)
+        return root
