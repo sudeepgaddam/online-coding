f2531184c09575bdd60b22c3024f267ff1408676
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-03-18 16:28:44 -0700

    Create 252_meeting_rooms.py

---
+++ leetcode_python/252_meeting_rooms.py
@@ -0,0 +1,17 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: bool
+        """
+        intervals.sort(key=lambda x:x.start)
+        for i in range(0,len(intervals)-1):
+            if intervals[i].end > intervals[i+1].start:
+                return False
+        return True
