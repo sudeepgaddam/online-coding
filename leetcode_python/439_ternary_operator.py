462db93c683c4678b5c49b86d80bcc7aa14b7b33
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-03-18 15:56:00 -0700

    Create 439_ternary_operator.py

---
+++ leetcode_python/439_ternary_operator.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def parseTernary(self, expression):
+        """
+        :type expression: str
+        :rtype: str
+        """
+        st = []
+        i = len(expression)
+        while i>0:
+            i-=1
+            if expression[i] =='?':
+                i-=1
+                n1 = st.pop()
+                n2 = st.pop()
+                if expression[i] == 'T':
+                    st.append(n1)
+                else:
+                    st.append(n2)
+            else:
+                if expression[i] not in [':']:
+                    st.append(expression[i])
+        return st.pop()
