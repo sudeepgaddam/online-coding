bdf5906eb42ad7c365767611affc26d4d0ae56ae
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-03-17 18:27:02 -0700

    Create 100_same_tree.py

---
+++ leetcode_python/100_same_tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        return (((p and q) and (p.val==q.val) and self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)) or (not p and not q))
