69f47c011d8792e8a34066bb21a6ac1245484005
Sudeep Reddy Gaddam <sudeepgaddam@gmail.com>
2019-04-04 20:19:34 -0700

    Update 75_sort_colors.py

--- leetcode_python/75_sort_colors.py
+++ leetcode_python/75_sort_colors.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        """
+        Time complexity: O(n)
+        Space complexity: O(1)
+        This method is based on insertion sort
+        """
         counter_arr=[0,0,0]
         for num in nums:
             counter_arr[num]+=1
